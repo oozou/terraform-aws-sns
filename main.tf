9304acd3696dc92e3a9b09dc4d474e412438b686
sedthawut tipkanpirome <sedthawut2000@gmail.com>
2022-07-12 16:25:28 +0700

    (update): new structure from aws provider

--- main.tf
+++ main.tf
@@ -1,96 +1,27 @@
 resource "aws_sns_topic" "this" {
-  name              = "${var.base_name}-${var.use_case}"
-  kms_master_key_id = aws_kms_key.service_key.arn
+  count = var.create_sns_topic ? 1 : 0
 
-  tags = merge({
-    Name = "${var.base_name}-${var.use_case}"
-  }, var.custom_tags)
-}
+  name        = var.name
+  name_prefix = var.name_prefix
 
-# Cloudformation stack complains of Invalid IAM role because of IAM eventual consistency to propagate to other services. Normally retry logic has been implicitly added to many aws resources in terraform provider like ECR but cloudformation_stack is still missing that.
-# Normally 1 minute should be good enough. Ref: https://github.com/terraform-providers/terraform-provider-aws/pull/1165#issue-130759721
-resource "time_sleep" "wait_1_minute" {
-  depends_on = [aws_iam_role.sns_subscription]
+  display_name                             = var.display_name
+  policy                                   = var.policy
+  delivery_policy                          = var.delivery_policy
+  application_success_feedback_role_arn    = var.application_success_feedback_role_arn
+  application_success_feedback_sample_rate = var.application_success_feedback_sample_rate
+  application_failure_feedback_role_arn    = var.application_failure_feedback_role_arn
+  http_success_feedback_role_arn           = var.http_success_feedback_role_arn
+  http_success_feedback_sample_rate        = var.http_success_feedback_sample_rate
+  http_failure_feedback_role_arn           = var.http_failure_feedback_role_arn
+  lambda_success_feedback_role_arn         = var.lambda_success_feedback_role_arn
+  lambda_success_feedback_sample_rate      = var.lambda_success_feedback_sample_rate
+  lambda_failure_feedback_role_arn         = var.lambda_failure_feedback_role_arn
+  sqs_success_feedback_role_arn            = var.sqs_success_feedback_role_arn
+  sqs_success_feedback_sample_rate         = var.sqs_success_feedback_sample_rate
+  sqs_failure_feedback_role_arn            = var.sqs_failure_feedback_role_arn
+  kms_master_key_id                        = var.kms_master_key_id
+  fifo_topic                               = var.fifo_topic
+  content_based_deduplication              = var.content_based_deduplication
 
-  create_duration = "1m"
-}
-
-# aws_sns_topic_subscription does not support email protocol because the endpoint needs to be authorized and does not generate an ARN until the target email address has been validated. This breaks the Terraform model and as a result are not currently supported.
-# The cloudformation stack below creates the sns subscription along with triggering the subscription emails. User will still need to manually confirm the subscription link in the emails though.
-resource "aws_cloudformation_stack" "sns_subscriptions" {
-  name          = "${var.base_name}-${var.use_case}"
-  template_body = local.subscription_template
-
-  iam_role_arn = aws_iam_role.sns_subscription.arn
-
-  count = signum(length(var.email_ids))
-
-  tags = merge({
-    Name = "${var.base_name}-${var.use_case}-sns-subscriptions"
-  }, var.custom_tags)
-
-  depends_on = [time_sleep.wait_1_minute]
-}
-
-resource "aws_sns_topic_policy" "this" {
-  arn = aws_sns_topic.this.arn
-
-  policy = data.aws_iam_policy_document.this.json
-}
-
-// fetch PowerUser SSO arn
-data "aws_iam_roles" "poweruser" {
-  name_regex  = "AWSReservedSSO_PowerUser_.*"
-  path_prefix = "/aws-reserved/sso.amazonaws.com/ap-southeast-1/"
-}
-
-data "aws_iam_policy_document" "this" {
-  statement {
-    sid    = "AllowPublishFromAWSService"
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = local.services_allowed_pricipals
-    }
-
-    actions = [
-      "SNS:Publish",
-    ]
-
-    resources = [aws_sns_topic.this.arn]
-
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceOwner"
-
-      values = [
-        data.aws_caller_identity.default.account_id,
-      ]
-    }
-  }
-
-  statement {
-    sid    = "AllowActionsForPowerUser"
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = concat(tolist([data.aws_caller_identity.default.arn]), tolist(data.aws_iam_roles.poweruser.arns))
-    }
-
-    actions = [
-      "SNS:Subscribe",
-      "SNS:SetTopicAttributes",
-      "SNS:RemovePermission",
-      "SNS:Receive",
-      "SNS:Publish",
-      "SNS:ListSubscriptionsByTopic",
-      "SNS:GetTopicAttributes",
-      "SNS:DeleteTopic",
-      "SNS:AddPermission",
-    ]
-
-    resources = [aws_sns_topic.this.arn]
-  }
+  tags = var.tags
 }
--- versions.tf
+++ versions.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.0"
     }
-    time = {
-      source  = "hashicorp/time"
-      version = ">= 0.3.0"
-    }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 1.0.0"
 }
